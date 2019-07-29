b4f0a430e620814f531c5cd7775aa1e5ff4b206c
Spencer Greff <recnepsspencer@gmail.com>
2019-07-29 01:04:36 -0600

    :tada: inital commit. Create-react-app. Auth utility, login component and some styling

---
+++ src/components/login.jsx
@@ -0,0 +1,11 @@
+// src/components/login.jsx
+
+import React from 'react'
+
+function Login(props) {
+    return <h1>Login to continue</h1>;
+}
+
+export default Login;
+
+
---
+++ src/containers/RootContainer.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+import { Wrapper } from "../components/Styles"
+
+import Login from "./Login";
+
+function RootContainer(){
+    return(
+        <Wrapper>
+            <Login />
+        </Wrapper>
+    );
+}
+
+export default RootContainer;
+
